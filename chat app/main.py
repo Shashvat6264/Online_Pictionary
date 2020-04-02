fab6c2d51cd3cdcc95236ab71ee2b4505489838f
Shashvat <shashvat6264@gmail.com>
2020-04-03 03:09:14 +0530

    Flas_Chat_App

---
+++ chat app/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+    return render_template('session.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+@socketio.on('paint_event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('recieve paint event: ' + str(json))
+    socketio.emit('paint_recieve', json, callback=messageReceived)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
