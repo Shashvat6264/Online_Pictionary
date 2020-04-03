6c2946b3e50d0619206f1041eeeb9802bbb5be58
Shashvat <shashvat6264@gmail.com>
2020-04-03 20:27:58 +0530

    Made Rooms

--- chat app/main.py
+++ chat app/main.py
@@ -1,33 +1,33 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
 @app.route('/')
-def sessions():
-    return render_template('session.html')
+def givetemp():
+    return render_template('index.html')
+
+
+@app.route('/<int:room>')
+def sessions(room):
+    return render_template('session.html', room=room)
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 @socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
+def handle_my_custom_event(json, method=['GET', 'POST']):
+    print('Joined' + str(json))
+    join_room(int(json['room']))
+    socketio.emit('print_msg',json, to=int(json['room']),callback=messageReceived)
 
-@socketio.on('my event')
+
+@socketio.on('send_msg')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-@socketio.on('paint_event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('recieve paint event: ' + str(json))
-    socketio.emit('paint_recieve', json, callback=messageReceived)
+    socketio.emit('print_msg', json, to=int(json['room']) ,callback=messageReceived)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
