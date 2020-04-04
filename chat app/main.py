b84c2c1432484b5b33f93f55ee1148f09e27adf5
Shashvat <shashvat6264@gmail.com>
2020-04-05 00:17:49 +0530

    User

--- chat app/main.py
+++ chat app/main.py
@@ -1,24 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_socketio import SocketIO, join_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+user_name = {}
 
 @app.route('/')
 def givetemp():
     return render_template('index.html')
 
+@socketio.on('user')
+def handle_my_custom_event(json,method=['GET', 'POST']):
+    print('User Registered'+str(json))
+    if json['room'] not in user_name.keys():
+        user_name[json['room']] = []
+    if json['username'] not in user_name[json['room']]:
+        user_name[json['room']].append(json['username'])
+    print(user_name)
 
-@app.route('/<int:room>')
-def sessions(room):
-    return render_template('session.html', room=room)
+
+@app.route('/<username>/<int:room>')
+def sessions(room, username):
+    return render_template('session.html', username=username, room=room)
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
 @socketio.on('join')
 def handle_my_custom_event(json, method=['GET', 'POST']):
+    # print(username)
     print('Joined' + str(json))
     join_room(int(json['room']))
     socketio.emit('print_msg',json, to=int(json['room']),callback=messageReceived)
